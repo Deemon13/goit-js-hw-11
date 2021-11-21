5ec98f2e2e70ccd44504ea7d009d90d604de2175
Dmitriy Fedorenko <dmitriy.fedorenko13@gmail.com>
2021-11-21 22:44:52 +0200

    without infinite scroll and styling

---
+++ src/js/utils.js
@@ -0,0 +1,57 @@
+import SimpleLightbox from 'simplelightbox';
+import getRefs from './get-refs';
+
+const refs = getRefs();
+
+// function for create gallery images markup
+
+function renderMarkup({ hits }) {
+  const markup = hits
+    .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+      return `
+      <a class="gallery__link" href="${largeImageURL}">
+        <div class="photo-card">
+            <img src="${webformatURL}" width="305" height="205" alt="${tags}" loading="lazy" />
+            <div class="info">
+              <p class="info-item">
+                <b>Likes</b>
+                ${likes}
+              </p>
+              <p class="info-item">
+                <b>Views</b>
+                ${views}
+              </p>
+              <p class="info-item">
+                <b>Comments</b>
+                ${comments}
+              </p>
+              <p class="info-item">
+                <b>Downloads</b>
+                ${downloads}
+              </p>
+            </div>
+        </div>
+        </a>
+        `;
+    })
+    .join('');
+  refs.galleryRef.insertAdjacentHTML('beforeend', markup);
+
+  let gallery = new SimpleLightbox('.gallery a');
+  gallery.refresh();
+}
+
+// function for smmoth scrolling
+
+function smoothScroll() {
+  const { height: cardHeight } = refs.galleryRef.firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+    top: cardHeight * 10,
+    behavior: 'smooth',
+  });
+}
+
+//default export
+
+export default { renderMarkup, smoothScroll };
